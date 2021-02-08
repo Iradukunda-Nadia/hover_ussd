0a7318183a022b639d32ea762e7e2957fb4f051d
Iradukunda-Nadia <41780932+Iradukunda-Nadia@users.noreply.github.com>
2021-02-08 14:44:52 +0300

    Create HoverUssdApi.java

--- android/src/main/java/com/lucdotdev/hover_ussd/HoverUssdApi.java
+++ android/src/main/java/com/lucdotdev/hover_ussd/HoverUssdApi.java
@@ -9,32 +9,26 @@ import android.content.IntentFilter;
 import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
 import com.hover.sdk.api.HoverParameters;
-
 import java.util.HashMap;
 import java.util.Map;
-
-
 public class HoverUssdApi {
-
-
     private Activity activity;
-
     public HoverUssdApi(Activity activity) {
         this.activity = activity;
     }
-
     ///This Start Listennig SMS before the begin of Transaction
     ///
     ///
 
 
 
+        public void sendUssd(String action_id, HashMap<String, String> extra, BroadcastReceiver smsReceiver) {
 
+            LocalBroadcastManager.getInstance(activity).registerReceiver(smsReceiver, new IntentFilter("com.lucdotdev.hover_ussd.SMS_MISS"));
         public void sendUssd(String action_id, HashMap<String, String> extra) {
 
             ///Initialize @HoverBuilder
             final HoverParameters.Builder builder = new HoverParameters.Builder(activity).request(action_id);
-
             ///If there are action with variables
             ///
             if (!extra.isEmpty()) {
@@ -42,10 +36,7 @@ public class HoverUssdApi {
                     builder.extra(entry.getKey(), entry.getValue());
                 }
             }
-
             Intent buildIntent = builder.buildIntent();
             activity.startActivityForResult(buildIntent, 0);
         }
-
-
     }
